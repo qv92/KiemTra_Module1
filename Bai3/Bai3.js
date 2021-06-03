a2f16d8a9c0313e355792b1fae2ca3b4b7fc939c
qv92 <83738706+qv92@users.noreply.github.com>
2021-06-03 09:54:33 +0700

    Update Bai3.js

--- Bai3/Bai3.js
+++ Bai3/Bai3.js
@@ -1,6 +1,6 @@
 function checkLetter(array) {
     let count = 0;
-    let a = "Không có"
+    let false
     for (let i=0;i<array.length;i++) {
         if ((array[i] == "a") || (array[i] == "e") || (array[i] == "i") || (array[i] == "o") || (array[i] == "u")) {
             count++;
@@ -9,6 +9,6 @@ function checkLetter(array) {
     if (count != 0) {
         return count;
     } else {
-        return a
+        return false
     }
 }
